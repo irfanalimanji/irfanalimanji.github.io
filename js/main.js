72ef4114bc6b2d7fdbd57c60a68b2d63177501a9
irfanalimanji <irf287@gmail.com>
2016-08-07 18:56:59 -0700

    newest

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+$("#resumeEducation").hide();
+$("#resumeExperience").hide();
+$("#profBlogBlog").hide();
+$("#profBlogInterviews").hide();
+
 $("#playButton").on("click", function(e){
 	e.preventDefault();
 	$(".work").fadeOut(function(){
-		$(".play").fadeIn();
+	$(".play").fadeIn();
 	});
-	$('body').attr('class', 'playStyle');
+	$("body").attr("class", "playStyle");
+	$("html").addClass("playHtml");
+	$("html").removeClass("workHtml");
 });
 
 $("#workButton").on("click", function(e){
@@ -15,14 +22,16 @@ $("#workButton").on("click", function(e){
 	$(".workLearn").hide();
 	$(".playLearn").hide();
 	$('body').attr('class', 'workStyle');
+	$("html").removeClass('playHtml');
+	$("html").addClass("workHtml");
 });
 
 $("#resumeButton").on("click", function(e){
 	e.preventDefault();
 	$("#profBlog").fadeOut(function(){
-	$("#resume").fadeIn()	
+	$("#resume").fadeIn()
 	});
-	
+
 });
 
 $("#profBlogButton").on("click", function(e){
@@ -31,3 +40,83 @@ $("#profBlogButton").on("click", function(e){
 	$("#profBlog").fadeIn()	
 	});
 });
+
+$("#travelButton").on("click", function(e){
+	e.preventDefault();
+	$("#foodBlog").fadeOut(function(){
+	$("#travel").fadeIn()
+	});
+});
+
+$("#foodBlogButton").on("click", function(e){
+	e.preventDefault();
+	$("#travel").fadeOut(function(){
+	$("#foodBlog").fadeIn()	
+	});
+});
+
+$("#wlButt").on("click", function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $("#resumeButton");
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $("body").animate({
+    scrollTop: targetOffset
+  }, 300);
+});
+
+
+$("#plButt").on("click", function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $("#travelButton");
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $("body").animate({
+    scrollTop: targetOffset
+  }, 300);
+});
+
+$("section nav").on("click", function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $("#worklearn");
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $("body").animate({
+    scrollTop: targetOffset
+  }, 300);
+});
+
+$("#headerEducation").on("click",function(){
+	$("#resumeEducation").fadeToggle();
+});
+
+$("#headerExperience").on("click",function(){
+  $("#resumeExperience").fadeToggle();
+});
+
+$("#headerBlogs").on("click",function(){
+	$("#profBlogBlog").fadeToggle();
+});
+
+$("#headerInterviews").on("click",function(){
+  $("#profBlogInterviews").fadeToggle();
+});
+
+$(".blogPic1").hover(function(){
+  $(".imgDescription1").fadeIn();
+})
